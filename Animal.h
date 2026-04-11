d890f2a7148f063a37bf460237e25fd3242714ab
malakanter60-wq <malakanter60@gmail.com>
2026-04-11 23:35:11 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../Core/Drawable.h"
+#include <string> // Added for string support
 
 enum ProductType
 {
@@ -8,47 +9,61 @@ enum ProductType
     MILK
 };
 
-class Animal :public Drawable
+class Animal : public Drawable
 {
-private:
-	string image_path;
+protected: // Changed to protected so Chick and Cow can access it easily
+    string image_path;
 public:
-
     virtual ProductType getProductType() const = 0;
     virtual int getProductValue() const = 0;
-	point curr_pos;
-	point curr_vel;
-	Animal(Game* r_pGame, point r_point, int r_width, int r_height, string img_path);
-	virtual void draw() const override;
-	virtual void moveStep() = 0;   //The action that should be taken each time step
+    
+    point curr_pos;
+    point curr_vel;
+
+    Animal(Game* r_pGame, point r_point, int r_width, int r_height, string img_path);
+    
+    virtual void draw() const override;
+    
+    // Aim: Make animals move randomly
+    virtual void moveStep() = 0;   
+    
+    // Aim: Put animal in random position on click
+    void setRandomPosition(int windowWidth, int windowHeight);
 };
 
 class Chick : public Animal
 {
 public:
-	Chick(Game* r_pGame, point r_point, int r_width, int r_height, string img_path);
-	virtual void moveStep();
+    Chick(Game* r_pGame, point r_point, int r_width, int r_height, string img_path);
+    
+    virtual ProductType getProductType() const override { return EGG; }
+    virtual int getProductValue() const override { return 5; } // Example value
+    
+    virtual void moveStep() override;
 };
 
 class Cow : public Animal
 {
 public:
-	Cow(Game* r_pGame, point r_point, int r_width, int r_height, string img_path);
-	virtual void moveStep();
+    Cow(Game* r_pGame, point r_point, int r_width, int r_height, string img_path);
+    
+    virtual ProductType getProductType() const override { return MILK; }
+    virtual int getProductValue() const override { return 10; } // Example value
+    
+    virtual void moveStep() override;
 };
 
+// Fixed typo "Drwable" to "Drawable"
 class Egg : public Drawable
 {
 public:
-	Egg(Game* r_pGame, point r_point, int r_width, int r_height, string img_path);
-	virtual void moveStep();
+    Egg(Game* r_pGame, point r_point, int r_width, int r_height, string img_path);
+    void draw() const override;
 };
 
-class Milk : public Drwable
+class Milk : public Drawable
 {
 public:
-	Milk(Game* r_pGame, point r_point, int r_width, int r_height, string img_path);
-	virtual void moveStep();
+    Milk(Game* r_pGame, point r_point, int r_width, int r_height, string img_path);
+    void draw() const override;
 };
-
-
