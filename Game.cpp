9cc846772970a3affdb3f65299fcd0ef2af97606
malakanter60-wq <malakanter60@gmail.com>
2026-04-11 23:45:45 +0200

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -2,195 +2,61 @@
 #include "../Config/GameConfig.h"
 #include <windows.h>
 
-Game::Game()
-{
-	pWind = CreateWind(config.windWidth, config.windHeight, config.wx, config.wy);
-
-	createToolbar();
-	createBudgetbar();
-
-	currentLevel = 1;
-	initLevel();
-	drawFoodArea();
-
-	clearStatusBar();
-}
-
-Game::~Game()
-{
-}
-
-clicktype Game::getMouseClick(int& x, int& y) const
-{
-	return pWind->WaitMouseClick(x, y);
-}
-
-string Game::getSrting() const
-{
-	string Label;
-	char Key;
-	keytype ktype;
-	pWind->FlushKeyQueue();
-	while (1)
-	{
-		ktype = pWind->WaitKeyPress(Key);
-		if (ktype == ESCAPE)
-			return "";
-		if (Key == 13)
-			return Label;
-		if (Key == 8)
-			if (Label.size() > 0)
-				Label.resize(Label.size() - 1);
-			else
-				Key = '\0';
-		else
-			Label += Key;
-		printMessage(Label);
-	}
-}
-
-window* Game::CreateWind(int w, int h, int x, int y) const
-{
-	window* pW = new window(w, h, x, y);
-	pW->SetBrush(config.bkGrndColor);
-	pW->SetPen(config.bkGrndColor, 1);
-	pW->DrawRectangle(0, 0, w, h);
-	return pW;
-}
-
-void Game::createToolbar()
-{
-	point toolbarUpperleft;
-	toolbarUpperleft.x = 0;
-	toolbarUpperleft.y = 0;
-
-	gameToolbar = new Toolbar(this, toolbarUpperleft, 0, config.toolBarHeight);
-	gameToolbar->draw();
-}
-
-void Game::createBudgetbar()
-{
-	point budgetbarUpperleft;
-	budgetbarUpperleft.x = 0;
-	budgetbarUpperleft.y = config.toolBarHeight;
-
-	gameBudgetbar = new Budgetbar(this, budgetbarUpperleft, 0, config.toolBarHeight);
-	gameBudgetbar->draw();
-}
-
-void Game::clearBudget() const
-{
-	pWind->SetPen(config.bkGrndColor, 1);
-	pWind->SetBrush(config.bkGrndColor);
-	pWind->DrawRectangle(config.windWidth - 500, config.toolBarHeight, config.windWidth, 2 * config.toolBarHeight);
-}
-
-void Game::printBudget(string msg) const
-{
-	clearBudget();
-
-	pWind->SetPen(config.penColor, 50);
-	pWind->SetFont(24, BOLD, BY_NAME, "Arial");
-	pWind->DrawString(config.windWidth - 200, config.toolBarHeight + 10, msg);
-}
-
-void Game::clearStatusBar() const
-{
-	pWind->SetPen(config.statusBarColor, 1);
-	pWind->SetBrush(config.statusBarColor);
-	pWind->DrawRectangle(0, config.windHeight - config.statusBarHeight, config.windWidth, config.windHeight);
-}
-
-void Game::printMessage(string msg) const
-{
-	clearStatusBar();
-	color textColor = LIGHTGRAY;
-	if (msg.find("Time Left") != string::npos && remainingTime <= 20)
-		textColor = RED;
-
-	pWind->SetPen(textColor, 50);
-	pWind->SetFont(28, BOLD, BY_NAME, "Arial");
-
-	pWind->DrawString(
-		10,
-		config.windHeight - (int)(0.85 * config.statusBarHeight),
-		msg
-	);
-}
-
-window* Game::getWind() const
-{
-	return pWind;
-}
-
-void Game::initLevel()
-{
-	if (currentLevel == 1)
-		remainingTime = 120;
-	else if (currentLevel == 2)
-		remainingTime = 90;
-	else
-		remainingTime = 60;
-}
-
-void Game::updateTimer()
-{
-	remainingTime--;
-
-	if (remainingTime <= 0)
-	{
-		printMessage("Time is up! You lost.");
-	}
-}
-
-void Game::drawFoodArea()
-{
-	window* pWind = getWind();
-
-	int yStart = 2 * config.toolBarHeight;
-
-	pWind->DrawImage(
-		"images\\grass.jpg",
-		0,
-		yStart,
-		config.windWidth,
-		config.windHeight - config.statusBarHeight
-	);
-}
+// ... [Keep your existing constructors and helper functions] ...
 
 void Game::letsgo()
 {
-	int x, y;
-	bool isExit = false;
+    int x, y;
+    bool isExit = false;
 
-	pWind->ChangeTitle("- - - - - - - - - - Farm Frenzy (CIE101-project) - - - - - - - - - -");
+    pWind->ChangeTitle("- - - - - - - - - - Farm Frenzy (CIE101-project) - - - - - - - - - -");
 
-	do
-	{
-		string budget_string = "BUDGET = $" + to_string(budget);
-		printBudget(budget_string);
+    // We use a counter to track 1 second (1000ms) while the loop runs every 10ms
+    int frameCounter = 0;
 
-		string timeStr = "Time Left: " + to_string(remainingTime);
-		printMessage(timeStr);
+    do
+    {
+        // 1. Update the UI Text
+        string budget_string = "BUDGET = $" + to_string(budget);
+        printBudget(budget_string);
 
-		Sleep(1000);
-		updateTimer();
-gameBudgetbar->updateAnimals();
+        string timeStr = "Time Left: " + to_string(remainingTime);
+        printMessage(timeStr);
 
-		if (remainingTime <= 0)
-			break;
+        // 2. AIM: Make animals move randomly 
+        // We call this every loop (every 10ms) for smooth animation
+        gameBudgetbar->updateAnimals();
 
-		if (pWind->GetMouseClick(x, y))
-		{
-			if (y >= 0 && y < config.toolBarHeight)
-			{
-				isExit = gameToolbar->handleClick(x, y);
-			}
-			else if (y >= config.toolBarHeight && y < 2 * config.toolBarHeight)
-			{
-				isExit = gameBudgetbar->handleClick(x, y);
-			}
-		}
+        // 3. Update the Timer (once per second)
+        frameCounter += 10; 
+        if (frameCounter >= 1000) {
+            updateTimer();
+            frameCounter = 0;
+        }
 
-	} while (!isExit);
+        if (remainingTime <= 0) break;
+
+        // 4. Handle Input
+        // Non-blocking check for mouse clicks
+        if (pWind->GetMouseClick(x, y))
+        {
+            // Toolbar 1 (Top)
+            if (y >= 0 && y < config.toolBarHeight)
+            {
+                isExit = gameToolbar->handleClick(x, y);
+            }
+            // Toolbar 2 (Budget/Animals) AND Field clicks
+            // This ensures "Random position on click" works when clicking the grass
+            else if (y >= config.toolBarHeight)
+            {
+                isExit = gameBudgetbar->handleClick(x, y);
+            }
+        }
+
+        // AIM: Keep movement fluid while saving CPU
+        Sleep(10); 
+
+    } while (!isExit);
 }
+
+// ... [Keep the rest of your functions like drawFoodArea and printMessage] ...
