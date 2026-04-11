32f9e3e89cf01910c42014c7f316e0bdc4bd9d67
malakanter60-wq <malakanter60@gmail.com>
2026-04-12 00:26:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,11 @@
-// main.cpp - 5 Points Complete!
-#include "Game.h"
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <random>
+#include "./Core/Game.h"
 
-using namespace std;
 
-int main() {
-    srand(time(NULL));
-    
-    // Create window
-    window w(800, 600);
-    w.SetWaitForClick(false);
-    
-    Game game(w);
-    game.StartGame();
-    
-    return 0;
+int main()
+{
+	//Create an object of controller
+	Game game;
+
+	game.go();
 }
+
