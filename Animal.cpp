68038e861cd0474b4bc15c7f6bd1833d292c498a
malakanter60-wq <malakanter60@gmail.com>
2026-04-11 21:22:24 +0200

    Update Animal.cpp

--- Animal.cpp
+++ Animal.cpp
@@ -31,15 +31,59 @@ void Chick::moveStep()
 	window* pWind = pGame->getWind();
 	pWind->DrawImage(image_path, RefPoint.x, RefPoint.y, width, height);
 	*/
-	cout << "Icon Chick Clicked" << endl;
+	
+    window* pWind = pGame->getWind();
+
+
+    pWind->SetBrush(WHITE);
+    pWind->SetPen(WHITE);
+    pWind->DrawRectangle(RefPoint.x, RefPoint.y, RefPoint.x + width, RefPoint.y + height);
+
+    
+    RefPoint.x += (rand() % 11) - 5; 
+    RefPoint.y += (rand() % 11) - 5;
+
+    
+    if (RefPoint.x < range_min_x)
+        RefPoint.x = range_min_x;
+    if (RefPoint.x > range_max_x)
+        RefPoint.x = range_max_x;
+
+    if (RefPoint.y < range_min_y)
+        RefPoint.y = range_min_y;
+    if (RefPoint.y > range_max_y)
+        RefPoint.y = range_max_y;
+
+    draw();
+
+	
 }
 
 Cow::Cow(Game* r_pGame, point r_point, int r_width, int r_height, string img_path) : Animal(r_pGame, r_point, r_width, r_height, img_path)
 {}
 
-void Cow::moveStep()
-{
-	//TO DO: add code for cleanup and game exit here
-	cout << "Icon Cow Clicked" << endl;
 
+	//TO DO: add code for cleanup and game exit here
+	void Cow::moveStep()
+{
+    window* pWind = pGame->getWind();
+
+    pWind->SetBrush(WHITE);
+    pWind->SetPen(WHITE);
+    pWind->DrawRectangle(RefPoint.x, RefPoint.y, RefPoint.x + width, RefPoint.y + height);
+
+    RefPoint.x += (rand() % 11) - 5;
+    RefPoint.y += (rand() % 11) - 5;
+
+    if (RefPoint.x < range_min_x)
+        RefPoint.x = range_min_x;
+    if (RefPoint.x > range_max_x)
+        RefPoint.x = range_max_x;
+
+    if (RefPoint.y < range_min_y)
+        RefPoint.y = range_min_y;
+    if (RefPoint.y > range_max_y)
+        RefPoint.y = range_max_y;
+
+    draw();
 }
