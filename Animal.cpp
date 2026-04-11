6664ff370624038d4eb18ac247fa4f57cd23a1af
uptr5x <166767315+uptr5x@users.noreply.github.com>
2026-04-11 22:40:25 +0200

    Update Animal.cpp

--- Animal.cpp
+++ Animal.cpp
@@ -107,3 +107,72 @@ int Cow::getProductValue() const
 
     draw();
 }
+
+Egg::Egg(Game* r_pGame, point r_point, int r_width, int r_height, string img_path) : Animal(r_pGame, r_point, r_width, r_height, img_path)
+{}
+
+int Egg::getProductValue() const
+{
+    return 10;
+}
+
+
+	//TO DO: add code for cleanup and game exit here
+	void Egg::moveStep()
+{
+    window* pWind = pGame->getWind();
+
+    pWind->SetBrush(WHITE);
+    pWind->SetPen(WHITE);
+    pWind->DrawRectangle(RefPoint.x, RefPoint.y, RefPoint.x + width, RefPoint.y + height);
+
+    RefPoint.x += (rand() % 11) - 5;
+    RefPoint.y += (rand() % 11) - 5;
+
+    if (RefPoint.x < range_min_x)
+        RefPoint.x = range_min_x;
+    if (RefPoint.x > range_max_x)
+        RefPoint.x = range_max_x;
+
+    if (RefPoint.y < range_min_y)
+        RefPoint.y = range_min_y;
+    if (RefPoint.y > range_max_y)
+        RefPoint.y = range_max_y;
+
+    draw();
+}
+
+Milk::Milk(Game* r_pGame, point r_point, int r_width, int r_height, string img_path) : Animal(r_pGame, r_point, r_width, r_height, img_path)
+{}
+
+
+int Milk::getProductValue() const
+{
+    return 10;
+}
+
+
+	//TO DO: add code for cleanup and game exit here
+	void Egg::moveStep()
+{
+    window* pWind = pGame->getWind();
+
+    pWind->SetBrush(WHITE);
+    pWind->SetPen(WHITE);
+    pWind->DrawRectangle(RefPoint.x, RefPoint.y, RefPoint.x + width, RefPoint.y + height);
+
+    RefPoint.x += (rand() % 11) - 5;
+    RefPoint.y += (rand() % 11) - 5;
+
+    if (RefPoint.x < range_min_x)
+        RefPoint.x = range_min_x;
+    if (RefPoint.x > range_max_x)
+        RefPoint.x = range_max_x;
+
+    if (RefPoint.y < range_min_y)
+        RefPoint.y = range_min_y;
+    if (RefPoint.y > range_max_y)
+        RefPoint.y = range_max_y;
+
+    draw();
+}
