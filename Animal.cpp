9e000c3724bf001164f479b3e99b3668d91d82ba
malakanter60-wq <malakanter60@gmail.com>
2026-04-18 15:30:42 +0200

    Update Animal.cpp

--- Animal.cpp
+++ Animal.cpp
@@ -3,12 +3,12 @@
 #include <iostream>
 
 // Base Animal Constructor
-Animal::Animal(Game* r_pGame, point r_point, int r_width, int r_height, string img_path) 
+Animal::Animal(Game* r_pGame, point r_point, int r_width, int r_height, string img_path)
     : Drawable(r_pGame, r_point, r_width, r_height)
 {
     image_path = img_path;
     curr_pos = r_point;
-    
+
     // AIM: Make animals move randomly (Initialize with a small random velocity)
     curr_vel.x = (rand() % 5) - 2; // Speed between -2 and 2
     curr_vel.y = (rand() % 5) - 2;
@@ -19,16 +19,13 @@ void Animal::draw() const
 {
     window* pWind = pGame->getWind();
     pWind->DrawImage(image_path, curr_pos.x, curr_pos.y, width, height);
-    if (type == CHICK)
-    pOut->DrawImage("chick.png", x, y);
-
-else if (type == COW)
-    pOut->DrawImage("cow.png", x, y);
+    
 }
 
 // --- Chick Implementation ---
 Chick::Chick(Game* r_pGame, point r_point, int r_width, int r_height, string img_path)
-    : Animal(r_pGame, r_point, r_width, r_height, img_path) {}
+    : Animal(r_pGame, r_point, r_width, r_height, img_path) {
+}
 
 void Chick::moveStep()
 {
@@ -39,7 +36,7 @@ void Chick::moveStep()
 
     // 2. Occasionally change direction randomly (Jitter)
     if (rand() % 50 == 0) {
-        curr_vel.x = (rand() % 3) - 1; 
+        curr_vel.x = (rand() % 3) - 1;
         curr_vel.y = (rand() % 3) - 1;
     }
 
@@ -50,13 +47,14 @@ void Chick::moveStep()
 
 // --- Cow Implementation ---
 Cow::Cow(Game* r_pGame, point r_point, int r_width, int r_height, string img_path)
-    : Animal(r_pGame, r_point, r_width, r_height, img_path) {}
+    : Animal(r_pGame, r_point, r_width, r_height, img_path) {
+}
 
 void Cow::moveStep()
 {
     // AIM: Make animals move randomly
     // Cows move slower than chicks generally
-    curr_pos.x += curr_vel.x * 0.5; 
+    curr_pos.x += curr_vel.x * 0.5;
     curr_pos.y += curr_vel.y * 0.5;
 
     if (rand() % 100 == 0) { // Changes direction less often than the chick
@@ -68,14 +66,3 @@ void Cow::moveStep()
     if (curr_pos.x < 0 || curr_pos.x > config.windWidth - width) curr_vel.x *= -1;
     if (curr_pos.y < config.toolBarHeight * 2 || curr_pos.y > config.windHeight - config.statusBarHeight - height) curr_vel.y *= -1;
 }
-
-// --- Product Implementations ---
-Egg::Egg(Game* r_pGame, point r_point, int r_width, int r_height, string img_path)
-    : Drawable(r_pGame, r_point, r_width, r_height) {}
-
-void Egg::moveStep() { /* Eggs don't move */ }
-
-Milk::Milk(Game* r_pGame, point r_point, int r_width, int r_height, string img_path)
-    : Drawable(r_pGame, r_point, r_width, r_height) {}
-
-void Milk::moveStep() { /* Milk doesn't move */ }
