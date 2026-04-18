ea939dacfd5bc1f2e08ca83960c3520a9bc4e451
malakanter60-wq <malakanter60@gmail.com>
2026-04-18 15:29:54 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -10,24 +10,25 @@ enum ProductType
 };
 
 class Animal : public Drawable
-{enum AnimalType { CHICK, COW };
-AnimalType type;
+{
+    enum AnimalType { CHICK, COW };
+    AnimalType type;
 protected: // Changed to protected so Chick and Cow can access it easily
     string image_path;
 public:
     virtual ProductType getProductType() const = 0;
     virtual int getProductValue() const = 0;
-    
+
     point curr_pos;
     point curr_vel;
 
     Animal(Game* r_pGame, point r_point, int r_width, int r_height, string img_path);
-    
+
     virtual void draw() const override;
-    
+
     // Aim: Make animals move randomly
-    virtual void moveStep() = 0;   
-    
+    virtual void moveStep() = 0;
+
     // Aim: Put animal in random position on click
     void setRandomPosition(int windowWidth, int windowHeight);
 };
@@ -36,10 +37,10 @@ class Chick : public Animal
 {
 public:
     Chick(Game* r_pGame, point r_point, int r_width, int r_height, string img_path);
-    
+
     virtual ProductType getProductType() const override { return EGG; }
     virtual int getProductValue() const override { return 5; } // Example value
-    
+
     virtual void moveStep() override;
 };
 
@@ -47,10 +48,10 @@ class Cow : public Animal
 {
 public:
     Cow(Game* r_pGame, point r_point, int r_width, int r_height, string img_path);
-    
+
     virtual ProductType getProductType() const override { return MILK; }
     virtual int getProductValue() const override { return 10; } // Example value
-    
+
     virtual void moveStep() override;
 };
 
